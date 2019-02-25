45a842ce2240485870157ea50c48028634d91b4a
Daniel Reverol <daniel.reverol@gmail.com>
2019-02-25 11:09:44 -0400

    sefe and soud

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { withFormik, Field } from 'formik';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App(props) {
+  const {
+    handleSubmit, isSubmitting, errors, isValid, touched,
+  } = props;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="row">
+        Email:
+        <Field
+          name="email"
+          type="email"
+          className="input"
+        />
+        {errors.email && touched.email
+          && <div className="error">{errors.password}</div>
+        }
       </div>
-    );
-  }
+      <div className="row">
+        Password:
+        <Field 
+          name="password" 
+          type="password" 
+          className="input" 
+        />
+        {errors.password && touched.email
+          && <div className="error">{errors.password}</div>
+        }
+      </div>
+      <div className="row">
+        <button
+          type="submit"
+          className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
+          disabled={isSubmitting}
+        >
+        Submit
+
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default App;
+export default withFormik({
+
+  validate(values) {
+    // arreglo de errores
+    const errors = {};
+
+    if (!values.password) {
+      errors.password = 'Password is required';
+    } else if (values.password.length < 8) {
+      errors.password = 'Password must be at least 8 characters';
+    }
+
+    return errors;
+  },
+
+  handleSubmit(values, formikBag) {
+    console.log(values);
+    formikBag.setSubmitting(false);
+  },
+})(App);
