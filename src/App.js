34de17a7fd946980e2519fe7f8347605102a6dd7
Daniel Reverol <daniel.reverol@gmail.com>
2019-02-25 12:01:02 -0400

    Formik with async validations

--- src/App.js
+++ src/App.js
@@ -1,67 +1,71 @@
 import React from 'react';
-import './App.css';
-import { withFormik, Field, ErrorMessage} from 'formik';
+import { withFormik, Field, ErrorMessage, Form } from 'formik';
 
-function App(props) {
-  const {
-    handleSubmit, isSubmitting, errors, isValid, touched,
-  } = props;
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="row">
-        Email:
-        <Field
-          name="email"
-          type="email"
-          className="input"
-        />
-        <ErrorMessage name="email" >
-          {message => <div className="error">{message}</div>}
-        </ErrorMessage>
-      </div>
-      <div className="row">
-        Password:
-        <Field 
-          name="password" 
-          type="password" 
-          className="input" 
-        />
-        <ErrorMessage name="password" >
-          {message => <div className="error">{message}</div>}
-        </ErrorMessage>
-      </div>
-      <div className="row">
-        <button
-          type="submit"
-          className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
-          disabled={isSubmitting}
-        >
-        Submit
+function MyForm(props) {
+    const {
+        isSubmitting,
+        isValid,
+    } = props;
 
-        </button>
-      </div>
-    </form>
-  );
+    return (
+        <Form>
+            <div className="row">
+                Email:
+                <Field name="email" type="email" className="input" />
+                <ErrorMessage name="email">
+                    {message => <div className="error">{message}</div>}
+                </ErrorMessage>
+            </div>
+
+            <div className="row">
+                Password:
+                <Field name="password" type="password" className="input" />
+                <ErrorMessage name="password">
+                    {message => <div className="error">{message}</div>}
+                </ErrorMessage>
+            </div>
+
+            <div className="row">
+                <button
+                    type="submit"
+                    className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
+                    disabled={isSubmitting || !isValid}
+                >
+                    Submit
+                </button>
+            </div>
+        </Form>
+    );
 }
 
 export default withFormik({
+    mapPropsToValues(props) {
+        return {
+            email: '',
+            password: '',
+        };
+    },
 
-  validate(values) {
-    // arreglo de errores
-    const errors = {};
+    async validate(values) {
+        const errors = {};
 
-    if (!values.password) {
-      errors.password = 'Password is required';
-    } else if (values.password.length < 8) {
-      errors.password = 'Password must be at least 8 characters';
-    }
+        if (!values.password) {
+            errors.password = 'Password is required';
+        } else if (values.password.length < 8) {
+            errors.password = 'Password must be at least 8 characters';
+        }
 
-    return errors;
-  },
+        await sleep(5000);
 
-  handleSubmit(values, formikBag) {
-    console.log(values);
-    formikBag.setSubmitting(false);
-  },
-})(App);
+        if (Object.keys(errors).length) {
+            throw errors;
+        }
+    },
+
+    handleSubmit(values, formikBag) {
+        formikBag.setSubmitting(false);
+        console.log(values);
+    },
+})(MyForm);
